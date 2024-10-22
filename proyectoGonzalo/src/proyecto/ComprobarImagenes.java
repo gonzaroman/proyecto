ef373c2e91a5dcc67585367b4fa2cc660f70c170
gonzaroman <152491175+gonzaroman@users.noreply.github.com>
2024-10-22 12:38:53 +0200

    subo nuevos cambios

--- proyectoGonzalo/src/proyecto/ComprobarImagenes.java
+++ proyectoGonzalo/src/proyecto/ComprobarImagenes.java
@@ -16,6 +16,9 @@ import org.jsoup.select.Elements;
  */
 public class ComprobarImagenes {
     String url;
+    
+     String rojo = "\u001B[31m"; //color rojo del mensaje en consola
+    String verde = "\u001B[32m"; //color rojo del mensaje en consola
 
     public String getUrl() {
         return url;
@@ -33,16 +36,23 @@ public class ComprobarImagenes {
         // 5. Imágenes con el atributo "alt" o "title"
         // Descargar y analizar el HTML de la página web
             try {
-            Document doc = Jsoup.connect(url).get();
+            Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
+        .get();
             Elements imgElements = doc.select("img");
             System.out.println("\nImágenes (alt y title):");
             for (Element img : imgElements) {
                 
                 String absUrl = img.absUrl("src");
                 String altText = img.attr("alt");
-                String titleText = img.attr("title");
+               // String titleText = img.attr("title");
                 System.out.println("ruta: " + absUrl);
-                System.out.println("Imagen: alt=\"" + altText + "\", title=\"" + titleText + "\"");
+                System.out.println("Imagen: alt=\"" + altText + "\"");
+                
+                if (altText.length()==0){
+                    System.out.println(rojo+"Error: Alt vacio");
+                }else{
+                    System.out.println(verde+"Correcto");
+                }
             }
         } catch (IOException iOException) {
         }
--- proyectoGonzalo/src/proyecto/Principal.java
+++ proyectoGonzalo/src/proyecto/Principal.java
@@ -28,23 +28,29 @@ public class Principal {
     /**
      * @param args the command line arguments
      */
+    
+    
     public static void main(String[] args) throws MalformedURLException {
          String url ="";
          String dominio="";
         // TODO code application logic here
          //url = "https://neoattack.com/";
          //url = "https://www.snsmarketing.es/";
-         // url = "https://www.snsmarketing.es/posicionamiento-seo/";
          // url = "https://www.snsmarketing.es/blog/tendencias-para-2025-chatbots-e-inteligencia-artificial-en-el-marketing-digital/";
          //url = "https://iesaugustobriga.educarex.es/";
          //url = "https://neoattack.com/agencia-de-fgjfggmarketing-de-contenidos/"; //esta es para probar errores 404
-        //  url ="https://www.snsmarketing.es/blog/tendesdfncias-para-2025-chatbots-e-inteligencia-artificial-en-el-marketing-digital/"; // redireccion 301
+        //  url ="http://www.template-joomspirit.com"; // redireccion 301
+        
+        String rojo = "\u001B[31m"; //color rojo del mensaje en consola
+    String verde = "\u001B[32m"; //color rojo del mensaje en consola
+        
          
         
         //PIDE URL POR TECLADO
         PedirUrl teclado = new PedirUrl();
         url = teclado.escribir();
-                  
+          
+          
          
          URL url1 = new URL(url);
          dominio = url1.getHost();
@@ -59,10 +65,15 @@ public class Principal {
          ComprobarEstado estadoURL = new ComprobarEstado(url);
          
          estadoURL.comprobar(); //devuelve true si la url está correcta, si es un 404 da error
-         estadoURL.devolverCodigoEstado();
+         estadoURL.devolverCodigoEstado(); //devuelve el codigo del estado, 200, 404
          estadoURL.devolverMensaje();
-         System.out.println(estadoURL.devolverCodigoEstado());
-         System.out.println(estadoURL.devolverMensaje());
+         //System.out.println(estadoURL.devolverCodigoEstado());
+         //System.out.println(estadoURL.devolverMensaje());
+         
+         if(estadoURL.comprobar()){
+             System.out.println(verde+"Conexión correcta: 200 ok");
+         
+         
          
          ComprobarEstructura EstructuraURL = new ComprobarEstructura(url);
          EstructuraURL.comprobar();
@@ -71,14 +82,22 @@ public class Principal {
          imagenesURL.comprobar();
         
          ComprobarEnlaces enlaces = new ComprobarEnlaces(url,dominio_y_protocolo);
-           enlaces.comprobar();
+         enlaces.comprobar();
            
          ComprobarTexto texto = new ComprobarTexto(url);
-         texto.comprobar();
+        texto.comprobar();
+        texto.calcularDensidad();
           
            
            
-           // System.out.println(doc.html());
+            //System.out.println(doc.html());
+            
+            
+            
+         }else{
+             System.out.println(rojo+"Error: url no encontrada: 404");
+             //Este error debe guardarse en base de datos
+         }
             
             
         
